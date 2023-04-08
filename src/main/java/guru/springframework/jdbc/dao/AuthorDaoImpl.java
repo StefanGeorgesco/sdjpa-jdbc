f1860a50c843e6482cc307e7450865d5427c9c4a
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-08 18:25:59 +0200

    Terminated branch

--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -3,13 +3,47 @@ package guru.springframework.jdbc.dao;
 import guru.springframework.jdbc.domain.Author;
 import org.springframework.stereotype.Component;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 /**
  * Created by jt on 8/20/21.
  */
 @Component
 public class AuthorDaoImpl implements AuthorDao {
+
+    private final DataSource source;
+
+    public AuthorDaoImpl(DataSource source) {
+        this.source = source;
+    }
+
     @Override
     public Author getById(Long id) {
+        Connection connection;
+        Statement statement;
+        ResultSet resultSet;
+
+        try {
+            connection = source.getConnection();
+            statement = connection.createStatement();
+            resultSet = statement.executeQuery("SELECT * FROM author where id = " + id);
+
+            if (resultSet.next()) {
+                Author author = new Author();
+                author.setId(id);
+                author.setFirstName(resultSet.getString("first_name"));
+                author.setLastName(resultSet.getString("last_name"));
+
+                return author;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
         return null;
     }
 }
