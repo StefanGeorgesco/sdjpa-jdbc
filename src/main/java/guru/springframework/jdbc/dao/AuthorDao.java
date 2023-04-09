9939a41bde918fbcec210ac5194de7f1f06f4a0e
Stefan Georgesco <stefan.georgesco@yahoo.fr>
2023-04-09 16:58:20 +0200

    Terminated branch

--- src/main/java/guru/springframework/jdbc/dao/AuthorDao.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDao.java
@@ -13,5 +13,7 @@ public interface AuthorDao {
 
     Author saveNewAuthor(Author author);
 
-    Author updateAuthor(Author saved);
+    Author updateAuthor(Author author);
+
+    void deleteAuthorById(Long id);
 }
--- src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
+++ src/main/java/guru/springframework/jdbc/dao/AuthorDaoImpl.java
@@ -115,27 +115,46 @@ public class AuthorDaoImpl implements AuthorDao {
     public Author updateAuthor(Author author) {
         Connection connection = null;
         PreparedStatement ps = null;
-        ResultSet resultSet = null;
 
         try {
             connection = source.getConnection();
-            ps = connection.prepareStatement("UPDATE author set first_name = ?, last_name = ? where author.id = ?");
+            ps = connection.prepareStatement("UPDATE author SET first_name=?, last_name=? WHERE id=?");
             ps.setString(1, author.getFirstName());
             ps.setString(2, author.getLastName());
             ps.setLong(3, author.getId());
             ps.execute();
-
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
             try {
-                closeAll(resultSet, ps, connection);
+                closeAll(null, ps, connection);
             } catch (SQLException e) {
                 e.printStackTrace();
             }
         }
 
-        return this.getById(author.getId());
+        return getById(author.getId());
+    }
+
+    @Override
+    public void deleteAuthorById(Long id) {
+        Connection connection = null;
+        PreparedStatement ps = null;
+
+        try {
+            connection = source.getConnection();
+            ps = connection.prepareStatement("DELETE FROM author WHERE id=?");
+            ps.setLong(1, id);
+            ps.execute();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                closeAll(null, ps, connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     private Author getAuthorFromRS(ResultSet resultSet) throws SQLException {
